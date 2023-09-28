7ec846de6b73fb9c33cce2e5c44f881dfbd3e89c
Jhane-Vivian <107698971+Jhane-Vivian@users.noreply.github.com>
2023-09-28 17:08:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+musica = '';
+
+function preload()
+{
+	musica = loadSound("music.mp3");
+}
+function tocar(){
+    musica.play();
+    musica.setVolume(1);
+    musica.rate(1);
+}
+
+function setup(){
+
+}
+function draw(){
+
+}
